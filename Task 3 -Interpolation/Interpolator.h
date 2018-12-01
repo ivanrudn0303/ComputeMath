41e50d114ca3a0ff303c60ee56fab858ecb82803
ivanrudn0303 <ivanrudn0303@gmail.com>
2018-12-01 17:18:06 +0300

    Create Interpolator.h

---
+++ Task 3 -Interpolation/Interpolator.h
@@ -0,0 +1,25 @@
+#ifndef INTERPOLATOR_H
+#define INTERPOLATOR_H
+#include <vector>
+
+struct Point
+{
+	double x, y;
+};
+
+class Interpolator
+{
+private:
+	std::vector<Point> Points;
+	bool Ready;
+	double* Coefficients;
+	uint32_t Size;
+	uint32_t Find(double) const;
+public:
+	Interpolator(const std::vector<Point>&);
+	Interpolator();
+	int SetUp();
+	int SetUp(const std::vector<Point>&);
+	double operator()(double) const;
+};
+#endif // ! INTERPOLATOR_H
