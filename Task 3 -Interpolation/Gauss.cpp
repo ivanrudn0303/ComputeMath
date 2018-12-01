7199d8421d614cccfa76dcaae58f954941060f91
ivanrudn0303 <ivanrudn0303@gmail.com>
2018-12-01 17:19:34 +0300

    Create Gauss.cpp

---
+++ Task 3 -Interpolation/Gauss.cpp
@@ -0,0 +1,75 @@
+#include "Gauss.h"
+
+double * GaussMatr(double * Data, uint32_t Dimension)
+{
+	if ((Data != nullptr) && Dimension)
+	{
+		for (uint32_t i = 0; i < Dimension; i++)
+		{
+			for (uint32_t j = Dimension - 1; j > i; j--)
+			{
+				double coeff = -Data[Dimension * i + j] / Data[Dimension * i + i];
+				for (uint32_t k = i; k < Dimension + 1; k++)
+					Data[k * Dimension + j] += Data[k * Dimension + i] * coeff;
+			}
+		}
+		double *res = new double[Dimension], sum = 0.0;
+		for (int64_t i = Dimension - 1; i >= 0; i--)
+		{
+			sum = Data[Dimension * Dimension + i];
+			for (uint32_t j = 1; j < Dimension - i; j++)
+			{
+				sum -= Data[(Dimension - j) * Dimension + i] * res[Dimension - j];
+			}
+			if (Data[i * Dimension + i] == 0)
+			{
+				delete[] res;
+				return nullptr;
+			}
+			res[i] = sum / Data[i * Dimension + i];
+		}
+		return res;
+	}
+	else
+		return nullptr;
+}
+
+double * ZeidelMatr(const double *Data, uint32_t Dimension)
+{
+	if ((Data != nullptr) && Dimension)
+	{
+		double *res = new double[Dimension], *p = new double[Dimension];
+		for (uint32_t i = 0; i < Dimension; i++)
+			res[i] = 0.0;
+		double SumSquare = 0.0;
+		uint32_t counter = 0;
+		do
+		{
+			SumSquare = 0.0;
+			for (uint32_t i = 0; i < Dimension; i++)
+				p[i] = res[i];
+			for (uint32_t i = 0; i < Dimension; i++)
+			{
+				double var = 0;
+				for (uint32_t j = 0; j < i; j++)
+					var += (Data[j * Dimension + i] * res[j]);
+				for (uint32_t j = i + 1; j < Dimension; j++)
+					var += (Data[j * Dimension + i] * p[j]);
+				res[i] = (Data[Dimension * Dimension + i] - var) / Data[i * Dimension + i];
+				SumSquare += (res[i] - p[i]) * (res[i] - p[i]);
+				counter++;
+			}
+
+		} while ((SumSquare > ACCURACY) && (counter < ITERATIONS));
+		delete[] p;
+		if (counter < ITERATIONS)
+			return res;
+		else
+		{
+			delete[] res;
+			return nullptr;
+		}
+	}
+	else
+		return nullptr;
+}
